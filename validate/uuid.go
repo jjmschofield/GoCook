ad454b4da167755bb6e700eedd504d13abaaabc9
Jack Schofield <jjmschofield@googlemail.com>
2018-07-31 13:31:31 +0100

    Making variable name more explicit

--- validate/uuid.go
+++ validate/uuid.go
@@ -5,10 +5,10 @@ import "gopkg.in/go-playground/validator.v9"
 func UuidString(uuid string) (isValid bool, validationMessage string){
 	validate := validator.New()
 
-	idValidationErr := validate.Var(uuid, "required,uuid4")
+	uuidValidationErr := validate.Var(uuid, "required,uuid4")
 
-	if idValidationErr != nil {
-		return false, idValidationErr.Error()
+	if uuidValidationErr != nil {
+		return false, uuidValidationErr.Error()
 	}
 
 	return true, ""
