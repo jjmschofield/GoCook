c58fede25088eeec2f307b99eb8d4361b329a81a
Jack Schofield <jjmschofield@googlemail.com>
2018-08-04 12:04:48 +0100

    Adding full recipe model and including steps, ingredients and tags

---
+++ api/recipes/models.go
@@ -0,0 +1,32 @@
+package recipes
+
+type Recipe struct {
+	// TODO - strings require sanitization
+	Id          string       `json:"id" validate:"omitempty,uuid4"`
+	Name        string       `json:"name" binding:"required" validate:"required,min=3,max=50"`
+	Description string       `json:"description" validate:"omitempty,min=0,max=2000"`
+	Time        Time         `json:"time"`
+	Yield       int          `json:"yield" validate:"omitempty,min=0"`
+	Steps       []Step       `json:"steps" validate:"required,dive"`
+	Ingredients []Ingredient `json:"ingredients" validate:"required,dive"`
+	Tags        []string     `json:"tags" validate:"required,dive,min=1,max=15"`
+	Url         string       `json:"url" validate:"omitempty,uri"`
+	ImgUrl		string		 `json:"imgUrl" validate:"omitempty,uri"`
+}
+
+type Ingredient struct {
+	Name string  `json:"name" binding:"required" validate:"required,min=0,max=50"`
+	Qty  float32 `json:"qty" validate:"omitempty,min=0,max=10000"`
+	Unit string  `json:"unit" validate:"omitempty,oneof=g mil cup tea table pinch"`
+}
+
+type Step struct {
+	Name        string `json:"name" binding:"required" validate:"required,min=0,max=50"`
+	Description string `json:"description" validate:"omitempty,min=0,max=2000"`
+	Time        Time   `json:"time"`
+}
+
+type Time struct {
+	PrepSec int `json:"prepSec" validate:"omitempty,min=0"`
+	CookSec int `json:"cookSec" validate:"omitempty,min=0"`
+}
--- api/recipes/recipe.go
+++
@@ -1,7 +0,0 @@
-package recipes
-
-type Recipe struct {
-	Id   string `json:"id" validate:"omitempty,uuid4"`
-	Name string `json:"name" binding:"required" validate:"required,min=0,max=50"`
-	Url  string `json:"url" validate:"omitempty,uri"`
-}
