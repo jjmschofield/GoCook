2dc39fa18e14202f9e70ec67c7b31ae513297301
Jack Schofield <jjmschofield@googlemail.com>
2018-08-05 17:21:24 +0100

    Syntax change

--- release-tasks.sh
+++ release-tasks.sh
@@ -8,7 +8,7 @@ echo "Preparing for release..."
 
 ## Database migrations
 echo "Getting github.com/golang-migrate/migrate binary..."
-curl -L https://github.com/golang-migrate/migrate/releases/download/v3.4.0/migrate.linux-amd64.tar.gz | tar xvz migrate
+curl -L https://github.com/golang-migrate/migrate/releases/download/v3.4.0/migrate.linux-amd64.tar.gz | tar xvz > migrate
 
 echo "Running migrations..."
 ./migrate -database $DB_CONNECTION --path sql/migrations up
