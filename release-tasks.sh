afe0e6c4236003358e1f3469ec12d8691363a8f4
Jack Schofield <jjmschofield@googlemail.com>
2018-08-06 01:46:52 +0100

    Dropping forced updates of swagger on CI as go does not seem to be available on path within heroku release phase / bash script

--- release-tasks.sh
+++ release-tasks.sh
@@ -13,13 +13,5 @@ curl -L https://github.com/golang-migrate/migrate/releases/download/v3.4.0/migra
 echo "Running migrations..."
 ./migrate.linux-amd64 -database $DB_CONNECTION --path sql/migrations up
 
-## Update Swagger
-echo "Getting swagger generator..."
-go get -u github.com/swaggo/swag/cmd/swag
-
-echo "Generating swagger..."
-swag init
-cp -p docs/swagger.json api/public/swagger.json
-
 ## Complete
 echo "Release preparation complete!"
