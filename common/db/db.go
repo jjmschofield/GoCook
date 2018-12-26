595fab170eaa6d26f94a980b8ca7b98eadfa43bc
Jack Schofield <jjmschofield@googlemail.com>
2018-12-26 19:37:38 +0000

    Adding generic handlers for single and multi row queries

--- common/db/db.go
+++ common/db/db.go
@@ -8,11 +8,11 @@ import (
 
 var dbPool *sql.DB
 
-func ConnectDb(connStr string){
+func ConnectDb(connStr string) {
 	db, err := sql.Open(
 		"postgres",
 		connStr,
-		)
+	)
 
 	if err != nil {
 		log.Fatal(err)
@@ -24,15 +24,55 @@ func ConnectDb(connStr string){
 
 	conErr := IsConnected()
 
-	if conErr != nil{
+	if conErr != nil {
 		log.Fatal(conErr)
 	}
 }
 
-func IsConnected() error{
+func IsConnected() error {
 	return dbPool.Ping()
 }
 
-func GetConnection() *sql.DB{
+func GetConnection() *sql.DB {
 	return dbPool
+}
+
+func SingleRowQuery(query string) (result []byte, err error) {
+	err = GetConnection().QueryRow(query).Scan(&result)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func MultiRowQuery(query string) (results [][]byte, err error) {
+	rows, queryErr := GetConnection().Query(query)
+
+	if queryErr != nil {
+		return nil, queryErr
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+
+		var thisResult []byte
+
+		err = rows.Scan(&thisResult)
+
+		if err != nil {
+			return nil, err
+		}
+
+		results = append(results, thisResult)
+	}
+
+	rowsErr := rows.Err()
+
+	if rowsErr != nil {
+		return nil, err
+	}
+	return results, err
 }
